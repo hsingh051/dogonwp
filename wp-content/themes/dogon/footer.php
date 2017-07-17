c12cdca3b8c72c7d68d7e329b67df03161a6a2e5
Harpreet Singh <er.hpreetsingh@gmail.com>
2017-07-17 12:18:28 +0530

    fixes

--- wp-content/themes/dogon/footer.php
+++ wp-content/themes/dogon/footer.php
@@ -8,7 +8,7 @@
 	<div class="footerbar">
 		<div class="container">
 			<p class="ftleft">©2017 Dog on Leash. All Rights Reserved.</p>
-			<p class="ftright">Montreal Web Design by PearlWhiteMedia.com</p>
+			<p class="ftright">Montreal Web Design by <a href="http://pearlwhitemedia.com">PearlWhiteMedia.com</a></p>
 		</div>	
 	</div>
 </footer>
